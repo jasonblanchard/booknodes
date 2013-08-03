b1a5b31f9e9146f2adc9cf8c268c25624c8ed20e
jasonblanchard <jas.blanchard@gmail.com>
2013-08-03 14:35:06 -0400

    Added method to aggregate all the idea nodes

--- app/models/book_event.rb
+++ app/models/book_event.rb
@@ -6,7 +6,10 @@ class BookEvent
   embeds_many :ideas
 
   def all_idea_nodes
-    self.ideas.map(&:nodes).flatten
+    self.ideas.map(&:nodes).flatten.inject(Hash.new(0)) do |h,v|
+      h[v] += 1
+      h
+    end
   end
 
 end
