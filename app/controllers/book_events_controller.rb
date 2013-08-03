a21305dafe28d7f3e77c0dd4df3b1d2c716d24ae
jasonblanchard <jas.blanchard@gmail.com>
2013-08-03 15:21:39 -0400

    Sorting by tag

--- app/controllers/book_events_controller.rb
+++ app/controllers/book_events_controller.rb
@@ -5,7 +5,13 @@ class BookEventsController < ApplicationController
 
   def show
     @event = BookEvent.find(params[:id])
-    @ideas = @event.ideas
+    
+    if params[:node]
+      @ideas = @event.ideas.order_by(:created_at => :desc).to_a.keep_if { |idea| idea.nodes.include?(params[:node]) }
+    else
+      @ideas = @event.ideas.order_by(:created_at => :desc).to_a
+    end
+
     @idea_list = @event.all_idea_nodes
   end
 
