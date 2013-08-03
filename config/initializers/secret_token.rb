5052755cfd878bdbc73e15dac19b42deacdc7381
jasonblanchard <jas.blanchard@gmail.com>
2013-08-02 20:13:44 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Booknotes::Application.config.secret_key_base = 'd129f2aaf1ce1b47c444a923910e24268ccc49d42c295a2415332aa36ca1e5db5b4f040debcb8fdedf04e8a831505bb2c9bdfc6db821048f9281a4002c82d758'
