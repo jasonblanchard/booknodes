fd6ea1e8e5ca58ce0d70a69f13da34e4bddcf687
jasonblanchard <jas.blanchard@gmail.com>
2013-08-07 18:29:13 -0400

    Added an index on ideas.nodes

--- app/models/book_event.rb
+++ app/models/book_event.rb
@@ -5,6 +5,7 @@ class BookEvent
   field :title
   belongs_to :user
   embeds_many :ideas
+  index "ideas.nodes" => 1
 
   def all_idea_nodes
     self.ideas.map(&:nodes).flatten.compact.inject(Hash.new(0)) do |h,v|
