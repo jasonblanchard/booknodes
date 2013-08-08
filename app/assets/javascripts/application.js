62333a2f6f75b4d6db1cc187a8bda374d011a04f
jasonblanchard <jas.blanchard@gmail.com>
2013-08-07 21:52:54 -0400

    Moved scrips to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,3 +30,38 @@ $(document).on('click.ideaEvents', 'a.expand-sidebar-button', function() {
   $('.mobile.sidebar').toggle("slide", { direction: "left" }, 'fast');
   return false;
 });
+
+  $(document).on('mouseenter', '.idea', function() {
+   $(this).find('.edit-button').show();
+ });
+
+  $(document).on('click', '.idea', function() {
+   $(this).find('.edit-button').show();
+ });
+
+  $(document).on('mouseleave', '.idea', function() {
+   $(this).find('.edit-button').hide();
+  });
+
+  $(document).on('dblclick', '.idea', function() {
+    $('.idea').removeClass('editing');
+    $(this).addClass('editing')
+  });
+
+  $(document).on('click', '.cancel-idea-form', function() {
+    $('.idea').removeClass('editing');
+    return false;
+  });
+
+  $(document).on('click', '.edit-button', function() {
+    $('.idea').removeClass('editing');
+    $(this).closest('.idea').addClass('editing')
+    return false;
+  });
+
+  $(document).on('keypress', '.idea', function(e) {
+    if (e.keyCode == 13) {
+      $(this).find("form").submit();
+      e.preventDefault();
+    }
+  });
