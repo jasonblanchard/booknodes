b783021d74a7106b8d534a97a507ed23225346a3
jasonblanchard <jas.blanchard@gmail.com>
2013-08-18 11:14:41 -0400

    Sorting list of nodes

--- app/models/book_event.rb
+++ app/models/book_event.rb
@@ -8,10 +8,21 @@ class BookEvent
   index "ideas.nodes" => 1
 
   def all_idea_nodes
+    sorted_idea_nodes
+  end
+
+  private
+
+  def get_all_idea_nodes_with_count
     self.ideas.map(&:nodes).flatten.compact.inject(Hash.new(0)) do |h,v|
       h[v] += 1
       h
     end
   end
+
+  def sorted_idea_nodes
+    Hash[*get_all_idea_nodes_with_count.sort_by { |k,v| k }.flatten]
+  end
+
 
 end
