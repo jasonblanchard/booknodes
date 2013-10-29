120f7f77efa7017ce3b95a2e8e7c06e9406a5ab7
Jason Blanchard <jas.blanchard@gmail.com>
2013-10-29 19:29:25 -0400

    Added styles for alerts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ Booknotes::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   Paperclip.options[:command_path] = "/usr/bin/convert"
 
