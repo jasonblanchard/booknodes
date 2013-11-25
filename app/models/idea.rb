a1f375df68855c383fa291133ea2a61f83351ddc
Jason Blanchard <jas.blanchard@gmail.com>
2013-11-25 13:05:20 -0500

    Added note

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -31,6 +31,7 @@ class Idea
     nodes.split(',').map { |v| v.strip.downcase }
   end
 
+  # TODO: Add hashtag syntax
   def self.split_nodes_from_note(string)
     string.scan(/{(.*?)}/).flatten.map { |v| v.strip.downcase }
   end
